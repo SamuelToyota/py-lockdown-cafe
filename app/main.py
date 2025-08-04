7a70ff69850faeb095f396e3b021b58f76f5d5d7
Samuel Yudi <toyotaa038@gmail.com>
2025-08-04 19:26:06 -0300

    Primeiro commit do projeto lockdown-cafe

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+from app.cafe import Cafe
+from app.errors import VaccineError, NotWearingMaskError
+
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
+    masks_to_buy = 0
+    try:
+        for friend in friends:
+            cafe.visit_cafe(friend)
+    except VaccineError:
+        return "All friends should be vaccinated"
+    except NotWearingMaskError:
+        masks_to_buy = sum(not friend.get("wearing_a_mask", False) for friend in friends)
+        if masks_to_buy > 0:
+            return f"Friends should buy {masks_to_buy} masks"
+    else:
+        return f"Friends can go to {cafe.name}"
